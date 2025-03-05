eeb509d58381e014f621cb5b2fd19136a77f46fc
Mahsa-jmorganLab <111803683+MahsaBank@users.noreply.github.com>
2025-03-05 16:15:44 -0600

    Update mitoPredictionsToVastSubs.py

--- mitoPredictionsToVastSubs.py
+++ mitoPredictionsToVastSubs.py
@@ -6,59 +6,62 @@ import os
 from skimage.measure import label, regionprops
 from scipy.io import savemat
 
-def process_sample(sample, slice_num, zarr_f, slice_name=None):
-    global diced_size, rowRange, colRange, threshold
-    
-    if slice_name is not None:
-       row_col = zarr_f[f"{slice_name}/xy_coordinates/{sample}"]
-    else:
-       row_col = zarr_f[f"xy_coordinates/{sample}"]
+
+def process_sample(sample, slice_name, zarr_f):
+    global diced_size, rowRange, colRange, mito_probability_threshold, pred_key_name, mipLevel, voxel_numbers_threshold
+
+    ds_factor = 2 ** mipLevel
+    row_col = sample.split("_")
     row, col = int(row_col[0]), int(row_col[1])
+    slice_num = int(slice_name) - 1
     
     if row in rowRange and col in colRange:
-        pred = zarr_f[f"pred/{sample}"]
-        pred = (np.array(pred) > threshold).astype("int")
+        pred = zarr_f[f"{slice_name}/{pred_key_name}/{sample}"]
+        pred = (np.array(pred) > mito_probability_threshold).astype("int")
         
         comps = label(pred, connectivity=1)
         props = regionprops(comps)
         
         vsub_list = []
         for prop in props:
-             vsub = []
-             idxs = np.where(comps == prop.label)
-             for i in range(len(idxs[0])):
-                 coors = [(row * diced_size[0] + idxs[0][i]) // 16, (col * diced_size[1] + idxs[1][i]) // 16, slice_num]
-                 vsub.append(coors)
-             vsub = np.array(vsub)
-             vsub = np.unique(vsub, axis=0)
-             if len(vsub) > 100:
+            
+            vsub = []
+            idxs = np.where(comps == prop.label)
+            vsub = np.column_stack([
+                (row * diced_size[0] + idxs[0]) // ds_factor,
+                (col * diced_size[1] + idxs[1]) // ds_factor,
+                np.full_like(idxs[0], slice_num)
+            ])
+            vsub = np.unique(vsub, axis=0)
+            if len(vsub) > voxel_numbers_threshold:
                 vsub_list.append(vsub)
-        
-        png_filename = f"{row}_{col}.png"
-        print(f"{png_filename} saved.")
+                
+        print(f"{sample} in plane {slice_name} completed.")
         
         return vsub_list
     return []
 
 if __name__ == "__main__":
     root_dir = "/storage1/fs1/jlmorgan/Active/morganLab/DATA/LGN_Developing/KxR_P11LGN/diced"
-    zarr_name_pattern = "predict_RGCdetection_plane*_checkpoint3900.zarr"
-    chk = zarr_name_pattern.split("_checkpoint")[-1].split(".zarr")[0]
-    slice_nums = range(749, 760)
-    threshold = 0.2
+    zarr_name = "predict_RGCdetection_plane*_checkpoint3900.zarr"
+    zarr_filename = os.path.join(root_dir, zarr_name)
+    save_name = 'vastSubs.mat'
+    save_filename = os.path.join(root_dir, save_name)
+    zarr_f = zarr.open(zarr_filename, 'r')
+    slice_names = list(zarr_f.keys())
+    mito_probability_threshold = 0.4
+    voxel_numbers_threshold = 100
+    pred_key_name = "pred"
     diced_size = [512, 512]
     startRow, stopRow = 0, 200
     startCol, stopCol = 0, 200
+    mipLevel = 4
     rowRange = range(startRow, stopRow + 1)
     colRange = range(startCol, stopCol + 1)
     all_slices_vastSubs = []
-    for slice_num in slice_nums:
-        zarr_filename = os.path.join(root_dir, zarr_name_pattern.replace("*", str(slice_num + 1)))
-        zarr_f = zarr.open(zarr_filename, 'r')
-        slice_name = None # "750"
-        sample_path = os.path.join(zarr_filename, "raw")
-        samples = [item for item in os.listdir(sample_path) if os.path.isdir(os.path.join(sample_path, item))]
-        args = [(sample, slice_num, zarr_f, slice_name) for sample in samples]
+    for slice_name in slice_names:
+        slice_keys = list(zarr_f[f"{slice_name}/raw"].keys())
+        args = [(sample, slice_name, zarr_f) for sample in slice_keys]
 
         with Pool(processes=cpu_count()) as pool:
             vastSubs = pool.starmap(process_sample, args)
@@ -67,9 +70,8 @@ if __name__ == "__main__":
         if isinstance(vastSubs, np.ndarray):
            vastSubs = vastSubs.tolist()
         all_slices_vastSubs.append(vastSubs)
-        print(f"vastSubs for plane {slice_num} is calculated.......")
+        print(f"vastSubs for plane {slice_name} is calculated.......")
 
-    save_filename = os.path.join(root_dir, f'vastSubs_planes{slice_nums}_thr{threshold}_chk{chk}.mat')
     savemat(save_filename, {'vastSubs': all_slices_vastSubs})
     print(f"vastSubs for planes {slice_nums} was saved in {save_filename}.")
 
