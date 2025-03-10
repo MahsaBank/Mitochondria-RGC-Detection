152e2265473ec4ef210a581374fb5a1713ae47b8
Mahsa-jmorganLab <111803683+MahsaBank@users.noreply.github.com>
2025-03-10 14:56:53 -0500

    Update predictionsToVastSubs.py

--- predictionsToVastSubs.py
+++ predictionsToVastSubs.py
@@ -1,18 +1,16 @@
 from multiprocessing import Pool, cpu_count
-from matplotlib import image
+from skimage.measure import label, regionprops
+from scipy.io import savemat
 import zarr
 import numpy as np
 import os
-from skimage.measure import label, regionprops
-from scipy.io import savemat
 
 
-def process_sample(sample, slice_name, zarr_f):
-    global diced_size, rowRange, colRange, mito_probability_threshold, pred_key_name, mipLevel, voxel_numbers_threshold
+def process_sample(sample, slice_name, zarr_f, params):
+    diced_size, rowRange, colRange, mito_probability_threshold, pred_key_name, mipLevel, voxel_numbers_threshold = params
 
     ds_factor = 2 ** mipLevel
-    row_col = sample.split("_")
-    row, col = int(row_col[0]), int(row_col[1])
+    row, col = map(int, sample.split("_"))
     slice_num = int(slice_name) - 1
     
     if row in rowRange and col in colRange:
@@ -35,41 +33,49 @@ def process_sample(sample, slice_name, zarr_f):
                 vsub_list.append(vsub)
                 
         print(f"{sample} in plane {slice_name} completed.")
-        
         return vsub_list
     return []
 
+
 if __name__ == "__main__":
+    # Configuration Parameters
     root_dir = "/storage1/fs1/jlmorgan/Active/morganLab/DATA/LGN_Developing/KxR_P11LGN/diced"
     zarr_name = "predict_RGCdetection_planes_range(78, 86)_checkpoint3900.zarr"
-    zarr_filename = os.path.join(root_dir, zarr_name)
     save_name = 'vastSubs.mat'
+    zarr_filename = os.path.join(root_dir, zarr_name)
     save_filename = os.path.join(root_dir, save_name)
+    
+    # Load Zarr file
     zarr_f = zarr.open(zarr_filename, 'r')
     slice_names = list(zarr_f.keys())
+
+    # Model Parameters
     mito_probability_threshold = 0.4
     voxel_numbers_threshold = 100
     pred_key_name = "pred"
     diced_size = [512, 512]
-    startRow, stopRow = 0, 200
-    startCol, stopCol = 0, 200
     mipLevel = 4
-    rowRange = range(startRow, stopRow + 1)
-    colRange = range(startCol, stopCol + 1)
+    rowRange = range(0, 201)
+    colRange = range(0, 201)
+    
+    # Store results
     all_slices_vastSubs = []
+    
+    # Prepare params for multiprocessing
+    params = (diced_size, rowRange, colRange, mito_probability_threshold, pred_key_name, mipLevel, voxel_numbers_threshold)
+    
     for slice_name in slice_names:
         slice_keys = list(zarr_f[f"{slice_name}/raw"].keys())
-        args = [(sample, slice_name, zarr_f) for sample in slice_keys]
+        args = [(sample, slice_name, zarr_f, params) for sample in slice_keys]
 
         with Pool(processes=cpu_count()) as pool:
             vastSubs = pool.starmap(process_sample, args)
     
         vastSubs = [vsub for vsub_list in vastSubs for vsub in vsub_list]
-        if isinstance(vastSubs, np.ndarray):
-           vastSubs = vastSubs.tolist()
-        all_slices_vastSubs.append(vastSubs)
+        all_slices_vastSubs.append(vastSubs if isinstance(vastSubs, list) else vastSubs.tolist())
+        
         print(f"vastSubs for plane {slice_name} is calculated.......")
 
+    # Save the results
     savemat(save_filename, {'vastSubs': all_slices_vastSubs})
     print(f"vastSubs for planes {slice_names} was saved in {save_filename}.")
-
